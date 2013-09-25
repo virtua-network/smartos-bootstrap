80a665dfbc469074967a8d12c8bc9372cd01ec6e
mguegan <mguegan@virtua.ch>
2013-09-25 16:30:57 +0200

    Remove step 2 that will be handled by salt-bootstrap

--- bootstrap-gz.sh
+++ bootstrap-gz.sh
@@ -6,7 +6,7 @@
 
 ### Variables ###
 BS_PKGIN_BASEURL="http://pkgsrc-eu-ams.joyent.com/packages/SmartOS/bootstrap"
-BS_PKGIN_VER="bootstrap-2013Q1-x86_64.tar.gz"
+BS_PKGIN_VER="bootstrap-2013Q2-x86_64.tar.gz"
 PKGIN_VTA_REPO="http://tornado.virtua.ch/smartos_local/packages/All"
 PKGIN_CNF_PATH="/opt/local/etc/pkgin/repositories.conf"
 BS_VERSION="2013Q1"
@@ -44,23 +44,13 @@ pkg_admin rebuild
 sed -i.orig "s/pkgsrc.joyent.com/pkgsrc-eu-ams.joyent.com/g" ${PKGIN_CNF_PATH}
 pkgin -fy up
 
-### Step 2. Install packages not -yet- distributed by Joyent
-echo "[*] STEP 2 - install packages that are not *yet* distributed by Joyent"
-pkgin -y in libuuid python27 py27-setuptools
-pkg_add ${PKGIN_VTA_REPO}/msgpack-0.5.7.tgz
-pkg_add ${PKGIN_VTA_REPO}/zeromq-3.2.3.tgz
-pkg_add ${PKGIN_VTA_REPO}/py27-zmq-2.2.0.1.tgz
-pkg_add ${PKGIN_VTA_REPO}/py27-m2crypto-0.21.1nb2.tgz
-pkg_add ${PKGIN_VTA_REPO}/py27-jinja2-2.6.tgz
-pkg_add ${PKGIN_VTA_REPO}/py27-msgpack-0.1.13.tgz
-
-### Step 3. Install Salt via bootstrap ###
-echo "[*] STEP 3 - Salt Stack install"
+### Step 2. Install Salt via bootstrap ###
+echo "[*] STEP 2 - Salt Stack install"
 curl -s -k -L ${BS_SALT_BASEURL}/${BS_SALT_VER} | \
-env BS_SALT_ETC_DIR=${BS_SALT_ETC_DIR} sh -s -- git ${BS_VERSION} 
+env BS_SALT_ETC_DIR=${BS_SALT_ETC_DIR} sh -s -- ${BS_VERSION} 
 
-### Step 4. Naming the Node ###
-echo "[*] STEP 4 - Naming Node"
+### Step 3. Naming the Node ###
+echo "[*] STEP 3 - Naming Node"
 sed -i.orig "s/\#id\:/id\:\ ${NODE_NAME}/" ${BS_SALT_ETC_DIR}/minion
 sed -i.orig "s/\#startup_states\:\ \'\'/startup_states\:\ \'highstate\'/" \
     ${BS_SALT_ETC_DIR}/minion
